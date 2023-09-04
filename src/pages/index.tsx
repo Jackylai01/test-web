1ca65fee503213817585128bc4cfeffb7a8d712f
jacky.inoby <sn185672@gmail.com>
2023-09-04 15:51:21 +0800

    feat(key-vision):  add new style setting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import KeyVision from '@components/Layout/ClientLayout/KeyVision';
 import Navbar from '@components/Layout/ClientLayout/Navbar';
 import type { NextPage } from 'next';
 const HomePage: NextPage = () => {
   return (
     <>
       <Navbar />
+      <KeyVision />
     </>
   );
 };
