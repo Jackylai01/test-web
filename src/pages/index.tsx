cd1e0e417118f08a6329222ce61c4c0e7ead64ee
jacky.inoby <sn185672@gmail.com>
2023-09-04 21:13:31 +0800

    feat(news): add news component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
-import KeyVision from '@components/Layout/ClientLayout/KeyVision';
 import Navbar from '@components/Layout/ClientLayout/Navbar';
+import News from '@components/Layout/ClientLayout/News';
 import type { NextPage } from 'next';
+
 const HomePage: NextPage = () => {
   return (
     <>
       <Navbar />
-      <KeyVision />
+      <News />
     </>
   );
 };
